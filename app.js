eca18563d6f53e047a3745b85b83ff4a81cdd00a
Archos67 <ghyzel.chris@gmail.com>
2014-05-10 14:42:55 -0400

    small change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict'
 /* 
  * Author: Christopher Ghyzel
- * Express server for testing out application on home servers
+ * Express server for testing out application on home computer
  * I whipped this up because there are no cross-origin requests for XMLHTTP.
  * 
  * To run just node app.js
